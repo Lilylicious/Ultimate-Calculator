d11b57f2b482ac9cfa16ef728961083c36a907f7
Dwargkiller <peter.hammerskog@gmail.com>
2015-05-31 03:20:37 +0200

    Gold Formulas

--- common/simulations/simulations.js
+++ common/simulations/simulations.js
@@ -5,7 +5,7 @@
 	==================================================
 */
 
-// DPS
+// Start DPS
 function baseDPS(index){
 /*
 Function not in use by other functions. Calculates the base DPS of any given hero (no upgrades, no gilds, no souls, just the DPS of level 1 of that hero)
@@ -81,5 +81,135 @@ function idleDPS(dps){
 }
 
 // End DPS
+// Start Gold
+
+function levelHero(hero,levels){
+
+	var start = hero.level;
+	
+	
+	for(var i=start;i<start+levels;i++){
+	
+	initLevel = hero.level;
+	
+	hero.level += 1;
+	
+	if(Math.floor(hero.level/1000)-Math.floor(initLevel/1000) >= 1 && hero.level < 4000) hero.dmgMult *= 10;
+	else if(Math.floor(hero.level/25)-Math.floor(initLevel/25) >= 1 && hero.level >= 200 && hero.level <=4100 && hero.has5x===false){ 
+	console.log(hero.name + " 4xd");
+	hero.dmgMult *= 4;
+	}
+	else if(Math.floor(hero.level/25)-Math.floor(initLevel/25) >= 1 && hero.level >= 200 && hero.level <=500 && hero.has5x===true) hero.dmgMult *= 4;
+	else if(Math.floor(hero.level/25)-Math.floor(initLevel/25) >= 1 && hero.level >= 525 && hero.level <=725 && hero.has5x===true) hero.dmgMult *= 5;
+	else if(Math.floor(hero.level/25)-Math.floor(initLevel/25) >= 1 && hero.level >= 750 && hero.level <=4100 && hero.has5x===true) hero.dmgMult *= 4;
+	}
+
+	return levelCost(start,hero.level,hero);
+}
+
+function simulateGold(data,secondCount){
+/*
+
+Monster gold: MonsterHP/15*min(3,1.025^max(0,Level-75)), rounded up. Treasure chests have a (10+MimzeeLevel*5) gold multiplier, applied before rounding. Bubos is taken into account here, Thusia is not.
+Treasure chest chance: (1+DoraLevel*0.2)%
+
+Take highest level completed current run. 
+
+Take idleDPS(totalDPS()), take MonsterHP. MonsterHP/totalDPS = seconds to kill. If boss and seconds > 30 + 5*Chronos, set gps to 0.
+
+*/
+
+//ASSUMES: Idling. No abilities.
+
+	console.log("Starts...");
+
+	readHeroes(data);
+		
+	var boss = {};
+	var normal = {};
+	var chest = {};
+	
+	var libBonus = 1;
+		
+		for(var i=1; i<Math.min(ancients[3].level+1,100);i++){
+			libBonus += 0.25 - 0.01 * Math.floor(i/10);
+		}
+		
+		libBonus += 0.15 * Math.max(0, ancients[3].level - 99);
+		
+		
+	
+	normal.level = data.highestFinishedZone+(4-data.highestFinishedZone%5);
+	boss.level = data.highestFinishedZone-(data.highestFinishedZone%5);
+	normal.hp = monsterHP(normal.level);
+	boss.hp = monsterHP(boss.level);
+	
+	var dps = idleDPS(totalDPS());
+
+	normal.timeToKill = normal.hp / dps;
+	boss.timeToKill = boss.hp / dps;
+	
+	normal.goldPerKill = normal.hp / 15*Math.min(3,Math.pow(1.025,Math.max(0,normal.level-75)));
+	boss.goldPerKill = boss.hp / 15*Math.min(3,Math.pow(1.025,Math.max(0,boss.level-75)));
+
+	normal.totalKills = (secondCount / (normal.timeToKill+0.5));
+	boss.totalKills = (secondCount / (boss.timeToKill+0.5));
+	
+	if(boss.timeToKill < 30 + (ancients[16].level*5)) boss.goldTotal = boss.goldPerKill * boss.totalKills;
+	else boss.goldTotal = 0;
+
+	normal.goldTotal = normal.goldPerKill * normal.totalKills;
+
+	//Treasure chests
+	
+	chest.count = normal.totalKills * (1+ancients[13].level*0.2)/100;
+	normal.fortunaCount = (ancients[11].level*0.0025) * normal.totalKills;
+	boss.fortunaCount = (ancients[11].level*0.0025) * boss.totalKills;
+
+	
+	normal.goldTotal -= chest.count * normal.goldPerKill;
+	normal.goldTotal += chest.count * (normal.goldPerKill * (1+ancients[8].level*0.5));
+	
+	normal.goldTotal -= normal.fortunaCount * normal.goldPerKill;
+	normal.goldTotal += normal.fortunaCount * normal.goldPerKill * 10;
+	
+	boss.goldTotal -= boss.fortunaCount * boss.goldPerKill;
+	boss.goldTotal += boss.fortunaCount * boss.goldPerKill * 10;
+	
+	normal.goldTotal *= libBonus * (1 + ancients[7].level * 0.05);
+	boss.goldTotal *= libBonus * (1 + ancients[7].level * 0.05);
+	
+	// output.value = "Gold per second: " + normal.level + ':' + (normal.goldTotal/secondCount).toPrecision(4) + ', ' + boss.level + ':' + (boss.goldTotal/secondCount).toPrecision(4);
+
+}
+
+// End Gold
+// Start Misc Sim
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
